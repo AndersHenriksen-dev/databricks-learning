79a7f3c5f63285935c108c4b07b1d010e2a299f5
Anders <anders-10@live.dk>
2025-11-29 20:32:43 +0100

    Working databricks asset bundle deployment configuration.

--- bundle/src/databricks_learning/simple.py
+++ bundle/src/databricks_learning/simple.py
@@ -1,3 +1,10 @@
 def square_sum(nums):
     """Simple function: returns sum of squares (easy to test)."""
     return sum(x*x for x in nums)
+
+
+def square_sum_runner(argv=None):
+    # simple entrypoint called from Databricks job
+    # For demo, compute and print
+    data = [1, 2, 3, 4]
+    print("sum of squares:", square_sum(data))
